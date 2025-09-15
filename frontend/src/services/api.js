f0584360d904a0fa081d07eaeaf5a048122b22db
siddhanthkapoor <kapoorsiddhanth15@gmail.com>
2025-09-15 12:35:31 +0530

    Update baseURL with real deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',  // Backend URL
+baseURL: 'https://taskify-backend-k14v.onrender.com',
 });
 
 // Add token to every request
